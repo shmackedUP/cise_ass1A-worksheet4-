d1e449da78a07f55e279a1e6c054735e29281272
shmackedUP <81208908+shmackedUP@users.noreply.github.com>
2021-08-10 11:25:10 +1200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Router } from 'react-router-dom';
+
+
+
+const App = () =>  {
+    return (
+        <Router>
+        <div>
+          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+            <ul className="header">
+                <li><a href = "/">Home</a></li>
+                <li><a href = "/SEPractice">Select the Practice</a></li>
+                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+            </ul>
+          <div className="content">
+            
+          </div>
+        </div>
+        </Router>
+    );
+}
+ 
+export default App;
